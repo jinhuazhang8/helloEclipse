d43a733848977fcad80c5c104f35fec1e45a2b9a
jinhuazhang8 <jinhua.zhang@unb.ca>
2016-10-23 17:39:53 -0300

    jinhuazhang

---
+++ cs2043Lab5/src/cs2043Lab5/InfoAPP.java
@@ -0,0 +1,11 @@
+package cs2043Lab5;
+
+public class InfoAPP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InfoPrintzhang s = new InfoPrintzhang();
+		s.printName();
+	}
+
+}
---
+++ cs2043Lab5/src/cs2043Lab5/InfoPrintzhang.java
@@ -0,0 +1,9 @@
+package cs2043Lab5;
+
+public class InfoPrintzhang {
+
+	public void printName(){
+		System.out.println("My name is jinhua Zhang!");
+	}
+
+}
